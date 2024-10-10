130b415cbb0b5690ae17b45d6c0fbde8af5f7a7a
Vuurbaard <sander@vuurbaard.com>
2024-10-10 11:44:14 +0000

    Invalidate session and delete session cookie on logout

--- src/routes/logout/+server.ts
+++ src/routes/logout/+server.ts
@@ -1,16 +1,15 @@
 import { fail, redirect } from "@sveltejs/kit";
-import { lucia } from "$lib/server/oauth";
+import { invalidateSession } from "$lib/server/sessions";
+
 import type { RequestEvent } from "@sveltejs/kit";
 
 export async function GET(event: RequestEvent): Promise<Response> {
 	if (!event.locals.session) {
 		redirect(302, "/login");
 	}
-	await lucia.invalidateSession(event.locals.session.id);
-	const sessionCookie = lucia.createBlankSessionCookie();
-	event.cookies.set(sessionCookie.name, sessionCookie.value, {
-		path: ".",
-		...sessionCookie.attributes
-	});
+
+	await invalidateSession(event.locals.session.id);
+	event.cookies.set("session", "", {	path: "/", maxAge: 0 });
+
 	return redirect(302, "/");
 }
